5542d71e41dbbd95471837961311b6cec46e588c
AirJonas <77889765+AirJonas@users.noreply.github.com>
2024-12-09 11:13:09 +0100

    started and fixed fetch method to get pokemon from poki api

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,30 +1,47 @@
-updateView()
+let pokemonObj=null
+let url = "https://pokeapi.co/api/v2/pokemon"
+let spriteField = document.getElementById("sprites");
 
-fetch("https://pokeapi.co/api/v2/pokemon/1")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+mainView()
 
-function updateView(){
+function mainView(){
     app.innerHTML= /*HTML*/ `
-    <div id="field">
-        <button onclick="getOnePokemon()">Find Pokemon</button>
-        <div id="sprites"></div>
-    </div>
+        <div id="field">
+            <div class="pokemonContainer">
+            </div>
+        </div>
+        <div class="buttonContainer">
+            <button onclick="catchPokemon()">Fang</button>    
+            <button onclick="updateView()">Finn en annen</button>
+            <button onclick="showPokemonView()">Vis dine pokemon</button>       
+        </div>
     `;
 }
 
 async function getOnePokemon(){
-    let
-    try{
-        const response = a
+    let randomNumber = Math.floor(Math.random() * 1024 + 1);
+    let pokemonContainer = document.querySelectorAll("pokemonContainer")
+    
+
+    url = "https://pokeapi.co/api/v2/pokemon/" + randomNumber; 
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const pokemon = await response.json();
+        let pokemonSprite = pokemon.sprites.front_default;
+        pokemonObj = {
+            name: pokemon.name,
+            sprite: pokemonSprite,
+            pokemonId: pokemon.id
+        }
+
+        
     }
-    let randomNumber = Math.floor(Math.random() * 1302)
-    let randomPokemon = fetch("https://pokeapi.co/api/v2/pokemon/1");
-    let spriteField = document.getElementById("sprites");
-    let pokemonSprite = randomPokemon.sprites.front_default;
-    console.log(randomPokemon);
-    spriteField.innerHTML += /*HTML*/`
-       <img src=${pokemonSprite}>
-   `;
+    catch(error){
+        console.error(error);
+    }
 }
+    
