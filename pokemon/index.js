5dffe710b9c3d5801811ae52d41cb764828541c4
AirJonas <77889765+AirJonas@users.noreply.github.com>
2024-12-10 13:41:17 +0100

    did som changes on showallmypokemon and implemented pokemon view

--- pokemon/index.js
+++ pokemon/index.js
@@ -8,8 +8,7 @@ mainView()
 function mainView(){
     app.innerHTML= /*HTML*/ `
         <div id="field">
-            <div id="pokemonContainer">
-            </div>
+            <div id="pokemonContainer"></div>
         </div>
         <div class="buttonContainer">
             <button onclick="catchPokemon()">Fang</button>    
@@ -19,11 +18,41 @@ function mainView(){
     `;
 }
 
+function showPokemonView(){
+    app.innerHTML = /*HTML*/`
+        <div id="boxContainer">
+            <div id="playerBox">${showAllMyPokemon()}</div>
+        </div>
+
+        <div class="buttonContainer">    
+            <button onclick="mainView()">Finn en pokemon</button>       
+        </div>
+    `;
+}
+
+function pokemonView(index){
+    app.innerHTML = /*HTML*/`
+        <div id="pokemonInfoBox">
+            <div id="myPokemonNameInfo">${myPokemon[index].name}</div>
+            <div id="myPokemonIDInfo">${myPokemon[index].pokemonId}</div>
+            <img id="myPokemonSpriteInfo" src="${myPokemon[index].sprite}">
+            <div id="pokemonTypesBox">
+                <div id="pokemonType1">${myPokemon[index].type1}</div>
+                <div id="pokemonType2"></div>
+            </div>
+        </div>
+        <div class="buttonContainer">    
+            <button onclick="mainView()">Finn en pokemon</button>
+            <button onclick="showPokemonView()">Vis dine pokemon</button>       
+        </div>
+
+    `;
+}
+
 async function getOnePokemon(){
     let randomNumber = Math.floor(Math.random() * 1024 + 1);
     let pokemonContainer = document.getElementById("pokemonContainer")
     
-
     url = "https://pokeapi.co/api/v2/pokemon/" + randomNumber; 
     try {
         const response = await fetch(url);
@@ -36,19 +65,32 @@ async function getOnePokemon(){
         pokemonObj = {
             name: pokemon.name,
             sprite: pokemonSprite,
-            pokemonId: "#"+pokemon.id
+            pokemonId: "#"+pokemon.id,
+            type1: pokemon.types[0].type.name
         }
         pokemonContainer.innerHTML = /*HTML*/`
             <div id="pokemonName">${pokemonObj.name}</div>
             <div id="pokemonID">${pokemonObj.pokemonId}</div>
             <img id="pokemonSprite" src="${pokemonSprite}">
         `;
-
-
     }
     catch(error){
         console.error(error);
     }
+}
+
+function showAllMyPokemon(){
+    let html = "";
+    for(let i = 0; i < myPokemon.length; i++){
+        html += /*HTML*/`
+            <div data-value=${i} onclick=pokemonView(this.dataset.value) class="pokemonsInBox">
+                <div class="myPokemonName">${myPokemon[i].name}</div>
+                <div class="myPokemonID">${myPokemon[i].pokemonId}</div>
+                <img class="myPokemonSprite" src="${myPokemon[i].sprite}">
+            </div>
+        `;
+    }
+    return html;
 }
 
 function catchPokemon(){
@@ -58,6 +100,7 @@ function catchPokemon(){
         myPokemon.push(pokemonObj);
         getOnePokemon()
     }
-    
 }
+
+
     
